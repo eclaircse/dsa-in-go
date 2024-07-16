99dcee4907a6e1d24474bf9f9fa5902f24ff2358
eclaircse <165996166+eclaircse@users.noreply.github.com>
2024-07-16 22:20:41 +0530

    Create leetcode-1-two-sum.go

---
+++ leetcode-1-two-sum.go
@@ -0,0 +1,18 @@
+func twoSum(nums []int, target int) []int {
+
+    // make a hashmap (num:index)
+    hashMap := make(map[int] int)
+
+    var ans []int // array to store the answer
+
+    for idx, num := range nums {
+        if existingIdx, ok := hashMap[target-num]; ok {
+            ans = append(ans, idx, existingIdx)
+            break
+        }
+
+        hashMap[num] = idx
+    }
+
+    return ans
+}
